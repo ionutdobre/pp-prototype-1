20744497d63efe0fa600d69e4f56e2a9552134e7
Ionut Dobre <dobreionut88@gmail.com>
2019-11-16 17:26:14 +0200

    adding logging

--- src/main/java/com/example/demo/pages/HomePage.java
+++ src/main/java/com/example/demo/pages/HomePage.java
@@ -2,6 +2,8 @@ package com.example.demo.pages;
 
 import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.wicketstuff.annotation.mount.MountPath;
 
 /**
@@ -11,8 +13,16 @@ import org.wicketstuff.annotation.mount.MountPath;
 @WicketHomePage
 @MountPath("/")
 public class HomePage extends BasePage<Void> {
+    private static final Logger logger = LoggerFactory.getLogger(HomePage.class);
 
     public HomePage(PageParameters parameters) {
         super(parameters);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        logger.info(">>> init!");
     }
 }
